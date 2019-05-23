b427234877f9cb530d91a10034454da679fa7a98
Michael Schwarz <michael.schwarz@iaik.tugraz.at>
2019-05-23 14:13:05 +0200

    removed obsolete comments

--- libattopng.c
+++ libattopng.c
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2017 Michael Schwarz.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "libattopng.h"
 #include <stdlib.h>
 #include <string.h>
--- libattopng.h
+++ libattopng.h
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2017 Michael Schwarz.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 /**
  * @file libattopng.h
  * @brief A minimal C library to write uncompressed PNG files.
